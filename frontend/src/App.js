ae23ee662e58a0619e35ac54de2ecd0faef0ecdf
fer <->
2022-03-24 23:43:05 -0300

    09 - Implementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,31 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+/**
+ * Explanation on why the structure of this file is diferrent from the one on the video:
+ * https://www.udemy.com/course/django-with-react-an-ecommerce-website/learn/lecture/24573312#questions/16168210
+ * 
+ * Documentation: https://reactrouter.com/docs/en/v6/getting-started/overview
+ */
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-        <main className='py-3'>
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            {" "}
+            <Route path="/" element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />}/>
+          </Routes>
+        </Container>
+      </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
