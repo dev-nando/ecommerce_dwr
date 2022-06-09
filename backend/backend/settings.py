61affba040bf91cbd3073809f4e218db3228dc7e
fer <->
2022-06-09 12:55:06 -0300

    17 - Static Files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,5 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/iamges/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
