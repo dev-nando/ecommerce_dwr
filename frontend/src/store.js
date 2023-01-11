992f630493fabac2f9c16b1ac2786c2cf5f7d28c
Fernando <fcechett@gmail.com>
2023-01-11 00:06:16 -0300

    Product List Reducer & Action. And also changes to the render method tha changed in React 18: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer
+    , 
+});
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState
-    , composeWithDevTools(applyMiddleware(...middleware)
-    ));
+const store = configureStore({
+    reducer: reducer
+    , preloadedState: initialState
+    , middleware: middleware
+});
 
 export default store;
