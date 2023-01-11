e036fd3a8648db3461502f2ce5eeabf8f7ba9ca7
Fernando <fcechett@gmail.com>
2023-01-11 12:59:11 -0300

    Bringing Redux Intro Home Screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,8 +4,8 @@ import {
     , PRODUCT_LIST_SUCCESS
     , PRODUCT_LIST_FAIL
 } from '../constants/productConstants'
-'diwmefwpi'
-const listProducts = () => async (dispatch) => {
+
+export const listProducts = () => async (dispatch) => {
     try{
         dispatch({ type: PRODUCT_LIST_REQUEST })
         const { data } = await axios.get('/api/products/')
