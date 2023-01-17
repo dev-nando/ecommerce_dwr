3b5099fd40f880526451a3fd68e37adb328d24e7
Fernando <fcechett@gmail.com>
2023-01-16 22:14:18 -0300

    Qty Select & Add To Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 /**
  * Explanation on why the structure of this file is diferrent from the one on the video:
@@ -24,6 +25,7 @@ function App() {
             {" "}
             <Route path="/" element={<HomeScreen />} />
             <Route path='/product/:id' element={<ProductScreen />}/>
+            <Route path='/cart/:id?' element={<CartScreen />}/>
           </Routes>
         </Container>
       </main>
