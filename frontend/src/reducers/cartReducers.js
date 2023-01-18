8374cb7a8dd5042daf18ae77596c025d70a74d83
Fernando <fcechett@gmail.com>
2023-01-17 22:59:46 -0300

    Add To Cart Functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,22 +1,22 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
-export const cartReducer = (state={cartItem:[]}, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
             const existItem = state.cartItems.find(x => x.product === item.product)
 
-            if (existItem){
-                return{
+            if (existItem) {
+                return {
                     ...state
                     , cartItems: state.cartItems.map(x =>
-                        x.product === existItem.product ? item : x
-                    )
+                        x.product === existItem.product ? item : x)
                 }
-            }else{
-                return{
+
+            } else {
+                return {
                     ...state
-                    , cartItems:[...state.cartItems, item]
+                    , cartItems: [...state.cartItems, item]
                 }
             }
 
